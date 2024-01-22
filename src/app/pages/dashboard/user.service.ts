eb9b08bd8226c60454f33d772cb1b071baf88fee
khaoula-RAJHI <79048613+khaoula-RAJHI@users.noreply.github.com>
2024-01-22 16:13:28 +0100

    Update user.service.ts

--- src/app/pages/dashboard/user.service.ts
+++ src/app/pages/dashboard/user.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs/internal/Observable";
 import { User } from "./user";
+import { environment } from 'src/environments/environment';
 
 @Injectable({
     providedIn: 'root'
