a690aa8665d051095eae2d14eeec57ac39ba9aa6
khaoula-RAJHI <79048613+khaoula-RAJHI@users.noreply.github.com>
2024-01-22 16:23:22 +0100

    Update auth.service.ts

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -9,7 +9,7 @@ import { environment } from 'src/environments/environment';
   providedIn: 'root'
 })
 export class AuthService {
-  private AUTH_API = environment.apiServerUrl + '/authentification';
+  private AUTH_API = environment.apiServerUrl + '/authentification/';
   private httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
   };
@@ -17,10 +17,10 @@ export class AuthService {
   constructor(private http: HttpClient) { }
 
   login(credentials: { username: string, password: string }): Observable<any> {
-    return this.http.post<any>(AUTH_API + 'signin', credentials, this.httpOptions);
+    return this.http.post<any>(this.AUTH_API + 'signin', credentials, this.httpOptions);
   }
 
   register(user: { username: string, email: string, password: string }): Observable<any> {
-    return this.http.post<any>(AUTH_API + 'signup', user, this.httpOptions);
+    return this.http.post<any>(this.AUTH_API + 'signup', user, this.httpOptions);
   }
 }
