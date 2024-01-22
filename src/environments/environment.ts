3d2b0218b472cb241dd7bc8f005a4070cbe9ac91
khaoula-RAJHI <79048613+khaoula-RAJHI@users.noreply.github.com>
2024-01-22 15:59:30 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:8081',
-  apiServerUrl: 'appecommercespring.azurewebsites.net',
+  apiServerUrl: 'https://appecommercespring.azurewebsites.net',
 };
 
 /*
