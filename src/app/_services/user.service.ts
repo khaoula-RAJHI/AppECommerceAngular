fca26258876bff7a6f254d70024743da65968ee7
khaoula-RAJHI <79048613+khaoula-RAJHI@users.noreply.github.com>
2024-01-22 16:11:49 +0100

    Update user.service.ts

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 //const API_URL = 'http://localhost:8081/api/test/';
 
